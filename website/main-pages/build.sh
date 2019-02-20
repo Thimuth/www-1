7fe083aba98ade4abd2db3fda7c4712a838d0e11
thimuth <thimuth@gmail.com>
2019-02-20 23:15:23 +0530

    Build added for the main pages

--- website/main-pages/build.sh
+++ website/main-pages/build.sh
@@ -3,8 +3,10 @@
 echo ".....Building main pages....."
 if [ $OSTYPE == "msys" ];
     then
+	mkdir -p $1
         mkdocs build && cp -r site/* $1/
     else
+	mkdir -p $1
         mkdocs build; rsync -ir site/ $1/;
 fi
 echo "....Completed building main pages...."
